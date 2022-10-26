00cf1fe51cb04f9c0b7eadcf86437f7fd736cd61
Marlon Beisner <72560673+MarHeroo@users.noreply.github.com>
2022-10-26 16:15:18 +0200

    wuddefuckhanischhiejemaeht

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,73 @@
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
-	private static Scanner scan = new Scanner(InputStream);
-	
+	private static Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		String car = getString();
-		List<String> parsedCar = parseString(car);
+		while (true){
+			parseString("ahhh");
+			String car = getString();
+			List<String> parsedCar = parseString(car);
+			outP(parsedCar);
+			System.out.println("Dein String enthält nur gültige Objekte: "+validateList(parsedCar));
+			System.out.println();
+		}
 	}
 	
 	public static String getString() {
-		System.out.println("Bitte gebe den zu parsenden String ein und dr�cke enter:");
+		System.out.println("Bitte gebe den zu parsenden String ein und dr�cke enter:");
+		System.out.println();
 		return scan.next();
 	}
 	
 	public static List<String> parseString(String st){
-		String[] partList = {"bl", "br", "fs", "hs", "tu", "fe", "sp"};
-		
 		List<String> ret = new List<String>();
-		if(st.length()/2==0) {
+		int futureListLength = st.length()/2;
+
+		if(st.length()%2==0) {
 			while(!st.equals("")) {
-				String subs = ""+st.charAt(st.length())+st.charAt(st.length()-1);
+				String subS = ""+st.charAt(st.length()-2)+st.charAt(st.length()-1);
+				st = st.substring(0, st.length()-2);
+				ret.append(subS);
 			}
 		}
-		
-		return ret;
+
+		return flipList(ret, futureListLength);
+	}
+
+	public static List<String> flipList(List<String> list, int listLength){
+		List<String> retList = new List<String>();
+
+		for (int i = 0; i < listLength; i++) {
+			list.toLast();
+			retList.append(list.getContent());
+			list.remove();
+		}
+
+		return retList;
+	}
+	
+	public static void outP(List<String> list){
+		int i = 1;
+		list.toFirst();
+		System.out.println("die Liste enthält die folgenden Objekte: ");
+		while(list.hasAccess()){
+			System.out.println(i+". "+list.getContent().toString());
+			list.next();
+			i++;
+		}
+	}
+
+	public static boolean validateList(List<String> list){
+		String[] partList = {"bl", "br", "fs", "hs", "tu", "fe", "sp"};
+		list.toFirst();
+		while(list.hasAccess()){
+			if(list.getContent().equals(partList[0])||list.getContent().equals(partList[1])||list.getContent().equals(partList[2])||list.getContent().equals(partList[3])||list.getContent().equals(partList[4])||list.getContent().equals(partList[5])||list.getContent().equals(partList[6])){
+				list.next();
+			}else{
+				return false;
+			}
+		}
+		return true;
 	}
 }
